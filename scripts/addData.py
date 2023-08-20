4ea23b976d227dc0962407e6c5c86e1478df906d
Ishaan Shah <70190533+ishaan812@users.noreply.github.com>
2023-08-20 18:19:11 +0530

    Revert Commit

--- scripts/addData.py
+++ scripts/addData.py
@@ -7,9 +7,9 @@ from csv import DictReader
 def get_db_conn():
     conn = psycopg2.connect(
         host="localhost",
-        database="attendance-management",
+        database="attendance2",
         user="postgres", # Replace postgres user name
-        password="8007ell") # Postgres password
+        password="hibye123") # Postgres password
     return conn
 
 def cleanup_all_tables(db_conn, table_list):
