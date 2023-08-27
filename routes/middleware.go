eb10c8d6bbbe7f2d1e1d1eb912cc49b38f4fa9ec
INSAEN <pratham1986@gmail.com>
2023-08-28 01:20:50 +0530

    middleware changes

--- routes/middleware.go
+++ routes/middleware.go
@@ -9,12 +9,23 @@ import (
 
 func jwtMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tokenString := r.Header.Get("Authorization")
-		if tokenString == "" {
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		tokenString, helloerror := r.Cookie("token")
+		// if tokenString == "" {
+		// 	http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		// 	return
+		// }
+		if helloerror != nil {
+			if helloerror == http.ErrNoCookie {
+				// If the cookie is not set, return an unauthorized status
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			// For any other type of error, return a bad request status
+			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		tokenStr := tokenString.Value
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
 			return []byte(os.Getenv("JWT_SECRET_KEY")), nil
 		})
 		if err != nil || !token.Valid {
